75e27bdeb5870ff307b54483e4c539c8be46653c
HenokAsaye <henokasaye77@gmail.com>
2026-05-02 01:04:11 +0300

    Refactor module structure: rename f1r3fly_block to block in proto.rs and update imports in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! F1r3fly block structure library.
 
+pub mod casper;
 pub mod builder;
 pub mod genesis;
 pub mod hashing;
@@ -11,9 +12,17 @@ pub mod validation;
 mod proto;
 
 pub use builder::{BlockBuildError, BlockBuilder, UnsignedBlock};
-pub use genesis::{GenesisConfig, WalletEntry};
-pub use hashing::{compute_block_hash, compute_bonds_map_hash, compute_deploy_hash, compute_header_hash, compute_post_state_hash};
+pub use casper::GhostForkChoice;
+pub use genesis::{ConfigError, GenesisConfig};
+pub use hashing::{
+	compute_block_hash, compute_bonds_map_hash, compute_deploy_hash, compute_post_state_hash,
+};
 pub use serialization::{BlockSerialize, SerializationError};
 pub use storage::{BlockStore, InMemoryBlockStore, StoreError};
-pub use types::*;
+pub use types::{
+	BlockBody, BlockHash, BlockHeader, BlockMessage, Bond, BondedValidatorInfo, BondsHash,
+	CommEvent, ConsumeEvent, DeployData, Event, Justification, PCost, ProcessedDeploy,
+	ProcessedSystemDeploy, ProduceEvent, PublicKey, Signature, StateHash, StateDagHash,
+	SystemDeploy,
+};
 pub use validation::{BlockLookup, BlockValidator, ValidationError};
--- src/proto.rs
+++ src/proto.rs
@@ -1,6 +1,6 @@
 /// Generated protobuf types.
 #[allow(clippy::all)]
 #[allow(dead_code)]
-pub mod f1r3fly_block {
-    include!(concat!(env!("OUT_DIR"), "/f1r3fly.block.rs"));
+pub mod block {
+    include!(concat!(env!("OUT_DIR"), "/block.rs"));
 }
